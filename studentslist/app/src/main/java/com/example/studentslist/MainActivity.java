bea80e310ee0e6385c6300bafd22e71fb6e2c1e9
redmoon <bsef19a507@pucit.edu.pk>
2022-05-26 01:27:15 +0500

    Updated code to add more students to list

--- studentslist/app/src/main/java/com/example/studentslist/MainActivity.java
+++ studentslist/app/src/main/java/com/example/studentslist/MainActivity.java
@@ -3,7 +3,10 @@ package com.example.studentslist;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
 
 import java.util.ArrayList;
@@ -12,14 +15,16 @@ public class MainActivity extends AppCompatActivity {
 
 
 
-
+    ArrayList<String> students = new ArrayList<String>();
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        ArrayList<String> students = new ArrayList<String>();
+
+
+
         setContentView(R.layout.activity_main);
         students.add("Lal Chand");
         students.add("Shahzaib");
@@ -30,6 +35,19 @@ public class MainActivity extends AppCompatActivity {
         ListView listView = (ListView) findViewById(R.id.student_list);
 
         listView.setAdapter(adapter);
+        Button addBtn = findViewById(R.id.add_btn);
+
+        addBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                EditText editText =  findViewById(R.id.input_item);
+                String data = editText.getText().toString();
+//                addBtn.setText(data);
+                students.add(data);
+                adapter.notifyDataSetChanged();
+
+            }
+        });
 
 
 
